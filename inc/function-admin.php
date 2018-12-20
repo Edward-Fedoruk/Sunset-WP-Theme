2dda0f3c6ecb239a46ce91eff132da531cf93851
Edward-Fedoruk <ed.fedorukk@gmail.com>
2018-12-20 16:46:42 +0200

    configured subpages

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -4,6 +4,7 @@
 
 function sunset_add_admin_page() {
 
+  // Generate Sunset page
   add_menu_page(
     "Sunset theme options",
     "Sunset",
@@ -13,10 +14,35 @@ function sunset_add_admin_page() {
     get_template_directory_uri() . "/img/sunset-icon.png",
     110 
   );
+
+  // Generate admin sub pages
+  add_submenu_page(
+    "edward_sunset", 
+    "Sunset theme options", 
+    "General", 
+    "manage_options", 
+    "edward_sunset", 
+    "sunset_theme_settings_page",
+    "sunset_theme_create_page"
+  );
+
+  add_submenu_page(
+    "edward_sunset",
+    "sunset CSS options",
+    "Custom CSS",
+    "manage_options", 
+    "edward_sunset_css",
+    "sunset_theme_settings_page"
+  );
+
 }
 
 add_action("admin_menu", "sunset_add_admin_page");
 
 function sunset_theme_create_page() {
   echo "<h1>Sunset Theme Options</h1>";
+}
+
+function sunset_theme_settings_page() {
+  return 0;
 }
