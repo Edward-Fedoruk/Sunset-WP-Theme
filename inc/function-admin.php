2786a57d5543434875c89e634fca5f299e034bf8
Edward-Fedoruk <ed.fedorukk@gmail.com>
2018-12-21 16:10:24 +0200

    added social fields

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -14,7 +14,7 @@ function sunset_add_admin_page() {
     get_template_directory_uri() . "/img/sunset-icon.png",
     110 
   );
-
+  
   // Generate admin sub pages
   add_submenu_page(
     "edward_sunset", 
@@ -22,9 +22,9 @@ function sunset_add_admin_page() {
     "General", 
     "manage_options", 
     "edward_sunset", 
-    "sunset_theme_settings_page",
     "sunset_theme_create_page"
   );
+
 
   add_submenu_page(
     "edward_sunset",
@@ -41,6 +41,11 @@ function sunset_add_admin_page() {
 
 function sunset_custom_settings() {
   register_setting("sunset-settings-group", "first_name");
+  register_setting("sunset-settings-group", "last_name");
+  register_setting("sunset-settings-group", "twitter_handler", "sunset_sanitize_twitter_handler");
+  register_setting("sunset-settings-group", "facebook_handler");
+  register_setting("sunset-settings-group", "gplus_handler");
+
   add_settings_section(
     "sunset-sidebar-options", 
     "Sidebar options", 
@@ -50,11 +55,61 @@ function sunset_custom_settings() {
 
   add_settings_field(
     "sidebar-name", 
-    "First Name", 
+    "Full Name", 
     "sunset_sidebar_name", 
     "edward_sunset",
     "sunset-sidebar-options"
   );
+  add_settings_field(
+    "sidebar-twitter", 
+    "Twitter", 
+    "sunset_sidebar_twitter", 
+    "edward_sunset",
+    "sunset-sidebar-options"
+  );
+  add_settings_field(
+    "sidebar-facebook", 
+    "Facebook", 
+    "sunset_sidebar_facebook", 
+    "edward_sunset",
+    "sunset-sidebar-options"
+  );
+  add_settings_field(
+    "sidebar-gplus", 
+    "Google+", 
+    "sunset_sidebar_gplus", 
+    "edward_sunset",
+    "sunset-sidebar-options"
+  );
+}
+
+// Sanitization settings
+function sunset_sanitize_twitter_handler($input) {
+  $output = sanitize_text_field( $input );
+  $output = str_replace("@", "", $output);
+  return $output;
+}
+
+
+function sunset_sidebar_facebook() {
+  $facebook = esc_attr( get_option('facebook_handler') );
+  echo '
+    <input type="text" name="facebook_handler" value="'.$facebook.'" placeholder="facebook" />
+  ';
+}
+
+function sunset_sidebar_gplus() {
+  $gplus = esc_attr( get_option('gplus_handler') );
+  echo '
+    <input type="text" name="gplus_handler" value="'.$gplus.'" placeholder="gplus" />
+  ';
+}
+
+function sunset_sidebar_twitter() {
+  $twitter = esc_attr( get_option('twitter_handler') );
+  echo '
+    <input type="text" name="twitter_handler" value="'.$twitter.'" placeholder="twitter" />
+  ';
 }
 
 function sunset_sidebar_options() {
@@ -63,7 +118,11 @@ function sunset_sidebar_options() {
 
 function sunset_sidebar_name() {
   $firstName = esc_attr( get_option('first_name') );
-  echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="first name" />';
+  $lastName = esc_attr( get_option('last_name') );
+  echo '
+    <input type="text" name="first_name" value="'.$firstName.'" placeholder="first name" />
+    <input type="text" name="last_name" value="'.$lastName.'" placeholder="last name" />
+  ';
 }
 
 add_action("admin_menu", "sunset_add_admin_page");
